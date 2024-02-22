23783820b92a1a0c321359b099aa789a8043cbf2
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-02-22 17:51:34 +0100

    Adding Beta Feedback

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,8 +2,4 @@ function createApp() {
     return require("express")()
 }
 
-// @ts-ignore
-console.log(__dirname)
-
 module.exports = { rateLimiter: require(__dirname + "/ratelimiter/ratelimiter.js"), quickfunctions: {createnewapp: createApp} } ;
-// I think this is making trouble
