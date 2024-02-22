8284b867d01ada18af5a1612beec9a07b8547e33
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-02-22 09:59:53 +0100

    not

--- middlewares/index.js
+++ middlewares/index.js
@@ -4,5 +4,6 @@ function createApp() {
 
 // @ts-ignore
 console.log(__dirname)
-module.exports = { rateLimiter: require(__dirname + "/middlewares/ratelimiter"), quickfunctions: {createnewapp: createApp} } ;
+
+module.exports = { rateLimiter: require(__dirname + "/ratelimiter/ratelimiter.js"), quickfunctions: {createnewapp: createApp} } ;
 // I think this is making trouble
