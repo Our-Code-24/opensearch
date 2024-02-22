93b91d92664c461e65d13a5c15507f7ff3104b5c
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-02-22 09:00:55 +0100

    Some more functions

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,1 +1,6 @@
-module.exports = { rateLimiterUsingThirdParty: require('./rateLimiter.js') } ;
+function createApp() {
+    return require("express")()
+}
+
+// @ts-ignore
+module.exports = { rateLimiter: require('./rateLimiter.js'), quickfunctions: {createnewapp: createApp} } ;
