75e4ab7ccfc9d892c5852ce74693ba92b29d00c8
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-02-22 09:54:33 +0100

    Add logging

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,5 +3,6 @@ function createApp() {
 }
 
 // @ts-ignore
+console.log(__dirname)
 module.exports = { rateLimiter: require(__dirname + "/middlewares/ratelimiter"), quickfunctions: {createnewapp: createApp} } ;
 // I think this is making trouble
