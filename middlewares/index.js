aa064b9df395ee5c54d7f17e5efc27b722f6f2e9
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-02-22 09:23:03 +0100

    Try implementing native

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,15 @@
+const rateLimit = require('express-rate-limit');
+
 function createApp() {
     return require("express")()
 }
 
 // @ts-ignore
-module.exports = { rateLimiter: require('./rateLimiter.js'), quickfunctions: {createnewapp: createApp} } ;
+module.exports = { rateLimiter: rateLimit({
+    windowMs: 60 * 60 * 1000, // 24 hrs in milliseconds
+    max: 100,
+    message: 'You have exceeded the 100 requests in 1 hr limit!', 
+    standardHeaders: true,
+    legacyHeaders: false,
+  }), quickfunctions: {createnewapp: createApp} } ;
 // I think this is making trouble
