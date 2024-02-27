c6710977770a883c61969083b554730d8e111f99
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-02-27 08:57:18 +0100

    Add /style.css instead of

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.get("/search", (req, res) => {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/style.css">
   </head>
   <body>
   <div id="overlay">
