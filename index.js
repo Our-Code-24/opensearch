74b037779068f1a6f42df70696eec16f42fd685d
stoppedwumm <mobileboxnews@icloud.com>
2024-04-02 18:39:06 +0200

    Changed to correct use

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env["AUTH_CLIENT_SECRET"],
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://opensearch-mu.vercel.app',
   clientID: process.env["AUTH_CLIENT_ID"],
   issuerBaseURL: 'https://opensearch-mu.eu.auth0.com'
 };
